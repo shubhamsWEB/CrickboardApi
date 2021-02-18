b3f1cef42d5aaf010ca1b99eec26c435f0550974
Shubham Agrawal <shubhama664@gmail.com>
2021-02-18 11:58:44 +0530

    Added api calls

--- axios.js
+++ axios.js
@@ -1,5 +1,12 @@
+// import axios from 'axios';
+// export default axios.create({
+//     baseURL: "http://192.168.0.169:3000/api",
+//     withCredentials:true
+// });
 import axios from 'axios';
-export default axios.create({
-    baseURL: "http://192.168.0.169:3000/api",
-    withCredentials:true
-});
+export const configAxios = (url) => {
+    return axios.create({
+        baseURL: url,
+        withCredentials: true
+    });
+}
