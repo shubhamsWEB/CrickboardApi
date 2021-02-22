8578289b5fa3b611dcb07da6d0b4d9b2ff6f6be3
Shubham Agrawal <shubhama664@gmail.com>
2021-02-22 10:19:43 +0530

    Bable config

--- axios.js
+++ axios.js
@@ -1,8 +1,3 @@
-// import axios from 'axios';
-// export default axios.create({
-//     baseURL: "http://192.168.0.169:3000/api",
-//     withCredentials:true
-// });
 import axios from 'axios';
 export const configAxios = (url) => {
     return axios.create({
